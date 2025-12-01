fbff224e88af516b0af84248c24ee7080cba3a70
mohamed alzarari <s2221189795@ku.edu.kw>
2025-12-01 18:08:09 +0300

    my pc git

---
+++ src/pages/DashboardPage.jsx
@@ -0,0 +1,33 @@
+import appointments from "./src/data/appointments"
+function DashboardPage(){
+    return(
+<div className="dashboard">
+    <h2>Appointments</h2>
+    <p>Today's schedule</p>
+
+    <div className="appointments-list">
+
+        {appointments.map((appt)=>(
+            <div className="appointment-card" key={appt.id}>
+
+                <div className="appointments-top">
+                <strong>{appt.patientName}</strong>
+                <span className="time">{appt.time}</span>
+                </div>
+            
+            </div>
+                <p>{appt.reason}</p>
+            <span className={`status ${appt.status.toLowerCase()}`}>{appt.status}</span>
+
+        <div className="actions">
+        <button className="view-btn">View</button>
+        {appt.status === "Upcoming" && ( <button className="cancel-btn">Cancel</button>)}
+    </div>
+
+  
+        ))}
+        </div>
+</div>
+    );
+}
+export default DashboardPage;
